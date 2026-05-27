1e72dcfd0fc8b0a2bf4dad6b0200e37534b0013b
loghmanhashemi <loghmanhashemi@gmail.com>
2026-05-27 11:50:23 +0330

    a comment added

--- cuda/simple_add.cpp
+++ cuda/simple_add.cpp
@@ -6,7 +6,7 @@ __global__ void add( int a, int b, int *c ) { // this code is run on gpu
 int main( void ) {
    int c;
    int *dev_c;
-   cudaMalloc( (void**)&dev_c, sizeof(int) ) ;
+   cudaMalloc( (void**)&dev_c, sizeof(int) ) ;// allocate vram: video ram (gpu memory)
    add<<<1,1>>>( 2, 7, dev_c ); //kernel<<<blocks, threads>>>(arguments);
    cudaMemcpy( &c,   dev_c,   sizeof(int),   cudaMemcpyDeviceToHost ) ;// host: cpu , device: gpu
    //printf( "2 + 7 = %d\n", *dev_c ); accessing device memory from cpu is buggy. the corrected version is below
