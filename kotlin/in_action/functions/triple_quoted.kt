47d310c6180b25199a7b33a980241f54b2828209
loghman <@l.hashemi>
2026-06-10 17:49:26 +0330

    tripple quoted

---
+++ kotlin/in_action/functions/triple_quoted.kt
@@ -0,0 +1,22 @@
+
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+    println("Dir: $directory, name: $fileName, ext: $extension")
+}
+
+fun parsePathRegexp(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("Dir: $directory, name: $filename, ext: $extension")
+    }
+}
+
+fun main(){
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+    parsePathRegexp("/Users/yole/kotlin-book/chapter.adoc")
+}
