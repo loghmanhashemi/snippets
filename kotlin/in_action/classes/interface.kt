0c27865d44a91370eaa48702482c249a4ece2f38
loghman <@l.hashemi>
2026-06-13 14:00:10 +0330

    interface

---
+++ kotlin/in_action/classes/interface.kt
@@ -0,0 +1,22 @@
+interface Clickable{
+    fun click()
+    fun showOff() = println("I am clickable!")
+}
+interface Focusable{
+    fun setFocus(b: Boolean) =
+        println(" I ${if (b) "got" else "lost"} focus")
+    fun showOff() = println("I am focusable!")
+}
+class Button :Clickable,Focusable{
+    override fun click() = println("I was clicked")
+    override fun showOff(){
+        super<Clickable>.showOff()
+        super<Focusable>.showOff()
+    }
+}
+fun main(){
+    val button = Button()
+    button.showOff()
+    button.setFocus(true)
+    button.click()
+}
