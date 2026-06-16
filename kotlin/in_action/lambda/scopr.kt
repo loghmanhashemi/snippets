41e7465d7eaaaee0ada44f613cb1d43375a0bf12
loghman <@l.hashemi>
2026-06-16 19:45:59 +0330

    lambda

---
+++ kotlin/in_action/lambda/scopr.kt
@@ -0,0 +1,9 @@
+fun printMessagesWithPrefix(messages: Collection<String>, prefix: String){
+     messages.forEach {
+        println("$prefix $it")
+    }
+}
+fun main(){
+    val errors = listOf("403 Forbidden", "404 Not Found")
+    printMessagesWithPrefix(errors, "Error:")
+}
