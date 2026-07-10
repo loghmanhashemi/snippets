d300c8a18984bb692d968a8170a40d7182481892
loghmanhashemi <loghmanhashemi@gmail.com>
2026-07-10 13:34:37 +0330

    comments added

--- kotlin/in_action/lambda/all.kt
+++ kotlin/in_action/lambda/all.kt
@@ -1,6 +1,6 @@
 data class Person(val name:String,val age:Int)
 fun main(){
    var people = listOf(Person("ALice",29),Person("Bob",31))
-   println(people.all({p :Person->p.age<27}))
+   println(people.all({p :Person->p.age<27}))// are all members less than 27
 }
 //false
