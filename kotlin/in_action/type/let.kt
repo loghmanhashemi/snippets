c254878add2857db49145b61a89f5dc5f5a9ca67
loghman <@l.hashemi>
2026-07-18 19:17:14 +0330

    let

---
+++ kotlin/in_action/type/let.kt
@@ -0,0 +1,10 @@
+fun sendEmailTo(email: String){ 
+    println("Sending email to $email")
+}
+fun main(){
+    var email : String? = "yole@example.com"
+    email?.let { sendEmailTo(it) }
+    email = null;
+    email?.let { sendEmailTo(it) } // nothing happens
+
+}
