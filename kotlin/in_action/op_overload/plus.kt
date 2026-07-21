4c89826ce29cbbdea9c0c9bed44728e14eae8f23
loghman <@l.hashemi>
2026-07-21 15:35:16 +0330

    plus

---
+++ kotlin/in_action/op_overload/plus.kt
@@ -0,0 +1,10 @@
+data class Point(val x: Int, val y: Int){
+    operator fun plus(other: Point) : Point{
+        return Point(x+other.x , y + other.y)
+    }
+}
+fun main(){
+    val p1 = Point(10,20)
+    val p2 = Point(15,10)
+    println(p1+p2)
+}
