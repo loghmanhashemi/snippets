e4b15c5afc706a31b965927e9b3165e12aefefd5
loghman <@l.hashemi>
2026-07-25 18:37:34 +0330

    big decimal

--- kotlin/in_action/op_overload/unary.kt
+++ kotlin/in_action/op_overload/unary.kt
@@ -3,7 +3,13 @@ data class Point(val x: Int, val y: Int)
 operator fun Point.unaryMinus():Point{
     return Point(-x,-y)
 }
+operator fun BigDecimal.inc() = this + BigDecimal.ONE
+
 fun main(){
     val p = Point(10,20)
     println(-p)
+    var bd = BigDecimal.ZERO
+    println(bd++) //0
+    println(++bd) //2
+
 }
