ba1cff29e017cb7127ed8eac1912071fb713c6e0
loghman <@l.hashemi>
2026-08-03 16:29:28 +0330

    bit manipulation

---
+++ kotlin/big_nerd/numbers/bit.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println(Integer.toBinaryString(42)) //converts an integer to binary representation
+    println(42.shl(2) )// shifts bits left
+    println(42.shr(2) )// shifts bits right
+    println(42.inv() )// invert bits
+}
