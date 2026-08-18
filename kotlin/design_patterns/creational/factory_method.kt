c7aa83c506f046507be93e46b668f89237043dd5
loghman <@l.hashemi>
2026-08-18 14:39:17 +0330

    factory_method

---
+++ kotlin/design_patterns/creational/factory_method.kt
@@ -0,0 +1,42 @@
+interface Button {
+    fun render()
+}
+class WindowsButton : Button {
+    override fun render() {
+        println("Rendering Windows button")
+    }
+}
+
+class HTMLButton : Button {
+    override fun render() {
+        println("Rendering HTML button")
+    }
+}
+abstract class Dialog {
+
+    // Factory Method
+    abstract fun createButton(): Button
+
+    fun render() {
+        val button = createButton()
+        button.render()
+    }
+}
+class WindowsDialog : Dialog() {
+    override fun createButton(): Button {
+        return WindowsButton()
+    }
+}
+
+class WebDialog : Dialog() {
+    override fun createButton(): Button {
+        return HTMLButton()
+    }
+}
+fun main() {
+    val dialog: Dialog = WindowsDialog()
+    dialog.render()
+
+    val webDialog: Dialog = WebDialog()
+    webDialog.render()
+}
