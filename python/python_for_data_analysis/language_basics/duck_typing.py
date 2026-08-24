dddd129f972cb9dd6e01a216c549607ff04d3f17
loghman <@l.hashemi>
2026-08-24 18:15:50 +0330

    duck_typing

---
+++ python/python_for_data_analysis/language_basics/duck_typing.py
@@ -0,0 +1,9 @@
+def isiterable(obj):
+    try:
+        iter(obj)
+        return True
+    except TypeError:
+        return False
+print(isiterable("a string"))
+print(isiterable([1,2,3]))
+print(isiterable(5))
