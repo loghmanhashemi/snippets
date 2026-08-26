9c67ae41c0f73c6baa9758d316ce0fbe37501174
loghman <@l.hashemi>
2026-08-26 18:03:58 +0330

    tuple

---
+++ python/python_for_data_analysis/data_structures/tuple.py
@@ -0,0 +1,10 @@
+#fixed-length, immutable sequence of Python objects
+tup1 = (4, 5, 6)
+tup2 = 4, 5, 6
+#You can convert any sequence or iterator to a tuple by invoking tuple:
+tup3=tuple([4, 0, 2])
+tup4 = tuple('string')
+print(tup1)
+print(tup2)
+print(tup3)
+print(tup4)
